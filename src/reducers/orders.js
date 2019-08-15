8daa7cef560a7dbcdddfa82a3260052024d157e1
mr.scrpt <mr.scrpt@gmail.com>
2019-08-15 11:36:00 +0300

    all tests completedd

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,6 +1,7 @@
-import { CREATE_NEW_ORDER } from '../modules/clients';
-import { MOVE_ORDER_NEXT, MOVE_ORDER_BACK } from '../actions/moveOrder';
-import { ADD_INGREDIENT } from '../actions/ingredients';
+import {CREATE_NEW_ORDER} from '../modules/clients';
+import {MOVE_ORDER_NEXT, MOVE_ORDER_BACK} from '../actions/moveOrder';
+import {ADD_INGREDIENT} from '../actions/ingredients';
+import ingredients from "../ingredients";
 
 // Реализуйте редьюсер
 // Типы экшенов, которые вам нужно обрабатывать уже импортированы
@@ -9,6 +10,72 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
   switch (action.type) {
+
+    case CREATE_NEW_ORDER :
+      return [
+        ...state, {...action.payload, "ingredients": [], position: "clients"}
+      ];
+
+    case MOVE_ORDER_NEXT :
+      return state.map(order => {
+        if (order.id === action.payload) {
+          switch (order.position) {
+            case 'clients':
+              return {...order, position: 'conveyor_1'};
+            case 'conveyor_1':
+              return {...order, position: 'conveyor_2'};
+            case 'conveyor_2':
+              return {...order, position: 'conveyor_3'};
+            case 'conveyor_3':
+              return {...order, position: 'conveyor_4'};
+            case 'conveyor_4':
+              if (order.ingredients.length){
+                return {...order, position: 'finish'};
+              } else {
+                return order;
+              }
+
+
+            default:
+              return order;
+          }
+        } else {
+          return order;
+        }
+      });
+      break;
+
+    case MOVE_ORDER_BACK :
+      return state.map(order => {
+        if (order.id === action.payload) {
+          switch (order.position) {
+            case 'conveyor_2':
+              return {...order, position: 'conveyor_1'};
+            case 'conveyor_3':
+              return {...order, position: 'conveyor_2'};
+            case 'conveyor_4':
+              return {...order, position: 'conveyor_3'};
+            default:
+              return order;
+          }
+        } else {
+          return order;
+        }
+      });
+      break;
+    case ADD_INGREDIENT:
+      return state.map(order=> {
+
+        if (order.position === action.payload.from){
+          return {...order, ingredients: [...order.ingredients, action.payload.ingredient]};
+        }
+        else {
+          return order;
+        }
+      });
+      break;
+
+
     default:
       return state;
   }
@@ -16,3 +83,9 @@ export default (state = [], action) => {
 
 export const getOrdersFor = (state, position) =>
   state.orders.filter(order => order.position === position);
+
+//...state, ...action.payload, "ingredients": [], position: "clients"};
+/*case MOVE_ORDER_NEXT : console.log(action); break;
+    case MOVE_ORDER_BACK : console.log(action); break;
+    case ADD_INGREDIENT : console.log(action); break;*/
+
